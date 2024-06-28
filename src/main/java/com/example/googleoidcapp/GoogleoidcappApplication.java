1b6372a8d3ffe62f3ae45445434a9a1a5da183cc
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-06-28 12:02:02 +0900

    spring initilizr

---
+++ src/main/java/com/example/googleoidcapp/GoogleoidcappApplication.java
@@ -0,0 +1,13 @@
+package com.example.googleoidcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleoidcappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleoidcappApplication.class, args);
+	}
+
+}
